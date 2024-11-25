d59de03a2332def7f27abde70e55013c6fdca3bf
Char0r0 <chao.zhang10@uon.edu.au>
2024-11-26 01:20:58 +1100

    update lambda

--- infrastructure/backup.tf
+++ infrastructure/backup.tf
@@ -52,7 +52,11 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.backup.arn
-    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+    events = [
+      "s3:ObjectCreated:Put",
+      "s3:ObjectCreated:Post",
+      "s3:ObjectRemoved:*"
+    ]
   }
 
   depends_on = [aws_lambda_permission.allow_s3]
